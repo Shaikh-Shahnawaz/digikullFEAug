7ac4e051e134c4f79ef021e03e4d77876255f24c
Shams Mahboob Islam <shamsme123@gmail.com>
2021-08-27 00:15:54 +0530

    Web Pack base code

---
+++ webpack/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  //Where files should be sent once they are bundled
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  //webpack 5 comes with devServer which loads in development mode
+  devServer: {
+    port: 5000,
+    writeToDisk: true,
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+  //Rules of how webpack will take our files, complie & bundle them for the browser
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html',
+    }),
+    new MiniCssExtractPlugin()
+  ],
+};
